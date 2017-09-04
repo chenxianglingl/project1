44db9acbfeec8199e30e2c616e961c4ec2a8d475
xiangling1 <854945355@qq.com>
2017-09-04 18:29:55 +0800

    取消eslint验证
    取消eslint验证

--- build/dev-server.js
+++ build/dev-server.js
@@ -86,9 +86,9 @@ console.log('> Starting dev server...')
 devMiddleware.waitUntilValid(() => {
   console.log('> Listening at ' + uri + '\n')
   // when env is testing, don't need open it
-  if (autoOpenBrowser && process.env.NODE_ENV !== 'testing') {
+  /*if (autoOpenBrowser && process.env.NODE_ENV !== 'testing') {
     opn(uri)
-  }
+  }*/
   _resolve()
 })
 
