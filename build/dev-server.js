41682c19bef33c2911d905d27976f3baf0bc6dd8
xiangling1 <854945355@qq.com>
2017-09-06 18:31:54 +0800

    增加自定义表单组件
    增加自定义表单组件

--- build/dev-server.js
+++ build/dev-server.js
@@ -63,7 +63,7 @@ app.use(hotMiddleware)
 
 // serve pure static assets
 var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory)
-app.use(staticPath, express.static('./static'))
+/*app.use(staticPath, express.static('./static'))
 const jsonServer = require('json-server')
 const apiServer = jsonServer.create()
 const apiRouter = jsonServer.router('db.json')
@@ -73,6 +73,35 @@ apiServer.use(middlewares)
 apiServer.use('/api', apiRouter)
 apiServer.listen(port+1, () => {
   console.log('JSON Server is running')
+})*/
+
+var apiServer = express()
+var bodyParser = require('body-parser')
+apiServer.use(bodyParser.urlencoded({ extended: true }))
+apiServer.use(bodyParser.json())
+var apiRouter = express.Router()
+var fs = require('fs')
+apiRouter.route('/:apiName')
+.all(function (req, res) {
+  fs.readFile('./db.json', 'utf8', function (err, data) {
+    if (err) throw err
+    var data = JSON.parse(data)
+    if (data[req.params.apiName]) {
+      res.json(data[req.params.apiName])  
+    }
+    else {
+      res.send('no such api name')
+    }
+    
+  })
+})
+apiServer.use('/api', apiRouter);
+apiServer.listen(port + 1, function (err) {
+  if (err) {
+    console.log(err)
+    return
+  }
+  console.log('Listening at http://localhost:' + (port + 1) + '\n')
 })
 
 var uri = 'http://localhost:' + port
--- src/components/slideShow.vue
+++ src/components/slideShow.vue
@@ -1,13 +1,80 @@
 <template>
-	<div>hello world {{x}}</div>
+	<div class="slide-show" @mouseover="clearInv" @mouseout="runInv">
+    <div class="slide-img">
+      <a :href="slides[nowIndex].href">
+        <transition name="slide-trans">
+          <img v-if="isShow" :src="slides[nowIndex].src" alt="">
+        </transition>
+        <transition name="slide-trans-old">
+          <img v-if="!isShow" :src="slides[nowIndex].src" alt="">
+        </transition>
+      </a>
+    </div>
+    <h2>{{slides[nowIndex].title}}</h2>
+    <ul class="slide-pages">
+      <li @click="goto(prevIndex)">&lt;</li>
+      <li v-for="(item, index) in slides" @click="goto(index)">
+        <a :class="{on: index === nowIndex}" href="">{{ index + 1 }}</a>
+      </li>
+      <li @click="goto(nextIndex)">&gt;</li>
+    </ul>
+  </div>
 </template>
 <script>
 	export default{
+    props:{
+      slides:{
+        type: Array,
+        default:[]
+      },
+      inv: {
+        type: Number,
+        default: 1000
+      }
+    },
 		data () {
 			return {
-				x: 1
+				nowIndex: 0,
+        isShow: true
 			}
-		}
+		},
+    computed: {
+      prevIndex () {
+        if(this.nowIndex === 0 ){
+          return this.slides.length-1
+        }else{
+          return this.nowIndex - 1
+        }
+      },
+      nextIndex () {
+        if(this.nowIndex === this.slides.length-1 ){
+          return 0
+        }else{
+          return this.nowIndex + 1
+        }
+      }
+    },
+    methods: {
+      goto (index) {
+        this.isShow = false;
+        setTimeout(() => {
+          this.isShow = true;
+          this.nowIndex = index;
+          this.$emit('onchange',index);
+        },10)
+      },
+      runInv () {
+        this.invId = setInterval(() => {
+          this.goto(this.nextIndex)
+        },this.inv)
+      },
+      clearInv () {
+        clearInterval(this.invId)
+      }
+    },
+    mounted () {
+      this.runInv()
+    }
 	}
 </script>
 <style scoped>
